3d4d416b7ff232a6c477311ad594d658c76ee6a9
Riyutokisaki <90891791+Riyutokisaki@users.noreply.github.com>
2021-11-26 17:41:10 +0800

    跳躍判定設置

--- test game/Assets/Scripts/NewController.cs
+++ test game/Assets/Scripts/NewController.cs
@@ -11,10 +11,34 @@ public class NewController : MonoBehaviour
     public float Speed = 3.5f;
     [Header("跳躍高度"), Range(0, 1500)]
     public float Jump = 300f;
+    [Header("檢查地板尺寸與位移"), Range(0, 1)]
+    public float CheckGroundRadius = 0.1f;
+    public Vector3 CheckGroundOffset;
+    [Header("跳躍按鍵與可跳躍圖層")]
+    public KeyCode keyJemp = KeyCode.Space;
+    public LayerMask Layer;
+
     #endregion
 
     //剛體元件
     private Rigidbody2D rig;
+
+    /// <summary>
+    /// 繪製圖示
+    /// 在unity繪製輔助用圖形
+    /// 線條、射線、圓形、方形、圖片
+    /// 圖示 Gizmos 類別
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        //1.決定顏色
+        //2.決定繪製圖形
+        //transform.position此物件的世界座標
+        Gizmos.color = new Color(1, 0, 0.2f,0.3f);
+        Gizmos.DrawSphere(transform.position + 
+           transform.TransformDirection(CheckGroundOffset), CheckGroundRadius);// transform.TransformDirection()根據變形元件的區域座標轉換為世界座標
+
+    }
 
     private void Start()
     {
@@ -30,6 +54,12 @@ public class NewController : MonoBehaviour
         Move();
     }
 
+    private void Update()
+    {
+        Flip();
+    }
+
+
     # region 方法
     /// <summary>
     /// 玩家是否移動
@@ -42,6 +72,27 @@ public class NewController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         //打印測試
         print("玩家左右按鍵值" + h);
+
+        rig.velocity = new Vector2(h * Speed, rig.velocity.y);//剛體元件.加速度 為 新的vector2(X軸為左右按鍵數值*速度,原先剛體加速度的垂直y數值)
     }
+
+
+    //翻面 h<往左Y軸角度180 h>往右y角度0
+    private void Flip()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if(h > 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+    }  
+    
+
+
     #endregion
 }
