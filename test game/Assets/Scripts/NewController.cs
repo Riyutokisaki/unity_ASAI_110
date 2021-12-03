04f8f32e48090822bb7186d197b2463076cfa45e
Riyutokisaki <90891791+Riyutokisaki@users.noreply.github.com>
2021-12-03 16:01:21 +0800

    角色C#補全
    加上動畫 跳躍

--- test game/Assets/Scripts/NewController.cs
+++ test game/Assets/Scripts/NewController.cs
@@ -17,11 +17,20 @@ public class NewController : MonoBehaviour
     [Header("跳躍按鍵與可跳躍圖層")]
     public KeyCode keyJemp = KeyCode.Space;
     public LayerMask Layer;
-
+    [Header("動畫參數")]//動畫控制器內的開關
+    public string Walk = "isWalk";
+    public string Up = "doTouch";
     #endregion
 
     //剛體元件
     private Rigidbody2D rig;
+    //動畫元件
+    [SerializeField]
+    private Animator an;
+    //將私人欄位顯示在屬性面板(不能更改)
+    [SerializeField]
+    //是否在地上(否)
+    private bool isGrounded;
 
     /// <summary>
     /// 繪製圖示
@@ -42,8 +51,9 @@ public class NewController : MonoBehaviour
 
     private void Start()
     {
+        //在遊戲開始時讀取物件的剛體與動畫控制器
         rig = GetComponent<Rigidbody2D>();
-
+        an = GetComponent<Animator>();
     }
    
     //Update 約60FPS
@@ -57,6 +67,8 @@ public class NewController : MonoBehaviour
     private void Update()
     {
         Flip();
+        CheckGround();
+        KeyJump();
     }
 
 
@@ -69,13 +81,12 @@ public class NewController : MonoBehaviour
     /// </summary>
     private void Move()
     {
-        float h = Input.GetAxis("Horizontal");
-        //打印測試
-        print("玩家左右按鍵值" + h);
-
+        float h = Input.GetAxis("Horizontal"); //按鍵輸入數值
         rig.velocity = new Vector2(h * Speed, rig.velocity.y);//剛體元件.加速度 為 新的vector2(X軸為左右按鍵數值*速度,原先剛體加速度的垂直y數值)
-    }
 
+        //當 水平值 不等於0(!=0) 勾選走路
+        an.SetBool(Walk, h != 0);
+    }
 
     //翻面 h<往左Y軸角度180 h>往右y角度0
     private void Flip()
@@ -92,7 +103,33 @@ public class NewController : MonoBehaviour
         }
     }  
     
+    private void CheckGround()
+    {
+        //偵測OnDrawGizmos畫出來的圓有沒有碰到地板
+        //Physics2D-2D偵測 Circle圓形(2D)
+        //碰撞資訊 = 2D 物理.覆蓋圓型(中心點,半徑,圖層)
+        Collider2D hit = Physics2D.OverlapCircle(transform.position +
+           transform.TransformDirection(CheckGroundOffset), CheckGroundRadius, Layer);
+        //print("碰到的圖層" + hit.name);
+        //將測定到的布林值觸發(是)
+        isGrounded = hit;
 
+        //當物件不在地上勾選跳躍
+        an.SetBool(Up, !isGrounded);
+
+    }
+    /// <summary>
+    /// 跳躍方法
+    /// </summary>
+    private void KeyJump()
+    {
+        //如果 在地板上 且 按下按鍵(空白鍵)
+        if (isGrounded && Input.GetKeyDown("space"))
+        {
+            //剛體名稱(在欄位有寫).添加推力(新的二維向量(X.Y))(向上 填寫Y)
+            rig.AddForce(new Vector2(0, Jump));
+        }
+    }
 
     #endregion
 }
