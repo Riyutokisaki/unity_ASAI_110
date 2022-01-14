ebd581a261c1aa89f79c91e6658e876ca6b83cb7
Riyutokisaki <90891791+Riyutokisaki@users.noreply.github.com>
2022-01-14 16:48:26 +0800

    1/14期末完成

--- test game/Assets/Scripts/FriendSpawner.cs
+++ test game/Assets/Scripts/FriendSpawner.cs
@@ -1,9 +1,9 @@
-
 using UnityEngine;
 
 public class FriendSpawner : MonoBehaviour
 {
     #region 欄位
+    [Header("生成速度")]
     public float maxtime = 8f;
     private float timer = 0f;
 
@@ -14,20 +14,20 @@ public class FriendSpawner : MonoBehaviour
 
     private void Awake()
     {
-        GameObject newfriend = Instantiate(friend[Random.Range(0,6)]);
-        newfriend.transform.position = transform.position + new Vector3(0, -4, 0);
+        GameObject newfriend = Instantiate(friend[Random.Range(0,6)]);//隨機實例化
+        newfriend.transform.position = transform.position + new Vector3(0, -4, 0);//生成位置
     }
 
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        if (timer > maxtime && h != 0)
+        float h = Input.GetAxis("Horizontal");//主角停止不動不生成
+        if (timer > maxtime && h != 0)//如果經過時間>CD時間&&H不等於0
         {
-            GameObject newfriend = Instantiate(friend[Random.Range(0, 6)]);
-            newfriend.transform.position = transform.position + new Vector3(0, -4, 0);
+            GameObject newfriend = Instantiate(friend[Random.Range(0, 6)]);//隨機實例化
+            newfriend.transform.position = transform.position + new Vector3(0, -4, 0);//生成位置
 
 
-            timer = 0;
+            timer = 0;//歸零
         }
         timer += Time.deltaTime;
     }
--- test game/Assets/Scripts/Partners.cs
+++ test game/Assets/Scripts/Partners.cs
@@ -15,11 +15,12 @@ public class Partners : MonoBehaviour
     #region 欄位
     [Header("抓到!")]
     public bool get = false;//Player碰到沒
-    public GameObject player;//碰觸對象
+    [Header("碰觸對象")]
+    public GameObject player;
+    [Header("玩家位置")]
     public Transform plTransform;
+    [Header("移動速度")]
     public float speed = 0.05f;
-    [Header("碰到之後等幾秒"), Range(0, 50)]
-    public float waitTime = 0.5f;
     [Header("遊戲管理")]
     public GameManager gameManager;
     [Header("動畫")]
@@ -38,12 +39,12 @@ public class Partners : MonoBehaviour
  
     private void Awake()
     {
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-        Scene scene= SceneManager.GetActiveScene();
-        if (scene.name == "故事模式") story = true;
-        AN = GetComponent<Animator>();
-        player = GameObject.Find("主角");
-        plTransform = player.transform;
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();//取得場景中的遊戲管理
+        Scene scene= SceneManager.GetActiveScene();//取得場景名稱
+        if (scene.name == "故事模式") story = true;//如果場景名稱等於"故事模式"bool開啟
+        AN = GetComponent<Animator>();//獲得動畫控制器
+        player = GameObject.Find("主角");//取得玩家物件
+        plTransform = player.transform;//取得玩家位置
         
         //tigger = GetComponent<Collider2D>().isTrigger;
     }
@@ -51,50 +52,47 @@ public class Partners : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //print("碰到了" + collision.gameObject.name);
-        if (collision.gameObject.name == "主角")
+        if (collision.gameObject.name == "主角")//如果碰到的是玩家
         {
-            AN.SetTrigger(jump);
-            get = true;
-            GetPartners();
+            AN.SetTrigger(jump);//播放跳躍動畫
+            get = true;//開啟BOOL(拿到了朋友)
+            GetPartners();//呼叫
         }
-        if (collision.gameObject == box && get)//若碰撞到的GameObject是box(此c#中障礙物的名稱)則
+        if (collision.gameObject == box && get)//若碰撞到的GameObject是box則
         {
-            AN.SetTrigger(die);
-            gameManager.GameOver();
+            AN.SetTrigger(die);//播放死亡動畫
+            gameManager.GameOver();//呼叫遊戲管理開GameOverUI
 
         }
-        if (collision.gameObject.tag == "Finish" && get)//若碰撞到的GameObject是box(此c#中障礙物的名稱)則
+        if (collision.gameObject.tag == "Finish" && get)//若碰撞到的GameObject在Finish圖層(FOR一般模式
         {
             AN.SetTrigger(die);
-            
             gameManager.GameOver();
 
         }
     }
     private void FixedUpdate()
     {
-        if (get)
+        if (get)//如過GET開著 重複執行
         {
             GetPartners();
         }
-        else if(!story)
+        else if(!story)//當關卡不等於故事模式(一般)他會像箱子移動
         {
             Move();
         }
     }
 
+    #region 方法
     void GetPartners()
     {
         //tigger=true;
-        transform.position = new Vector3(plTransform.position.x - distance, plTransform.position.y, Time.deltaTime);
+        transform.position = new Vector3(plTransform.position.x - distance, plTransform.position.y, Time.deltaTime);//位置移動到玩家後面
         //RB.MovePosition(plTransform.position);
         float h = Input.GetAxis("Horizontal");
-        if (h != 0) AN.SetTrigger(walk);
-        if (transform.position.y > 0)
-        {
-            AN.SetTrigger(jump);
-           
-        }
+        if (h != 0) AN.SetTrigger(walk);//移動動畫播放
+        if (transform.position.y > 0) AN.SetTrigger(jump);//高於一定高度播放跳躍
+       
     }
 
     void Move()
@@ -110,5 +108,5 @@ public class Partners : MonoBehaviour
             transform.position += Vector3.right * speed;
         }
     }
-
+    #endregion
 }
